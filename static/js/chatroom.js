a4a086652cc70576e4007cbffc2e128233cc9fd1
zhangke <zhangke>
2018-08-06 13:18:27 +0800

    perfect ui

--- static/js/chatroom.js
+++ static/js/chatroom.js
@@ -1,5 +1,5 @@
 $(function () {
-
+    var headImg = imgarr[sj(imgarr.length)];
     $.ajax({
         url:"http://" + window.location.host + "/name",
         method:"GET",
@@ -7,12 +7,13 @@ $(function () {
         success:function (data) {
             // Create a socket
             socket = new WebSocket('ws://' + window.location.host + '/ws?name=' + data.name);
-            $("#name").html(data.name);
-            // Message received on the socket
+            //title
+            $(".tit").html("欢迎 "+data.name);
+            // Receive msg
             socket.onmessage = function (event) {
                 var data = JSON.parse(event.data);
                 console.info(data)
-                $("#display").append("<div>"+data.Name+" : "+data.Context+"</div>");
+                left(headImg,data.Name+" : "+data.Context);
 
             };
 
@@ -23,9 +24,43 @@ $(function () {
                 $('#sendbox').val('');
             }
 
-            $('#sendbtn').click(function () {
+            //enable input ?
+            $('.footer').on('keyup','input',function(){
+                if($(this).val().length>0){
+                    $(this).next().css('background','#114F8E').prop('disabled',true);
+
+                }else{
+                    $(this).next().css('background','#ddd').prop('disabled',false);
+                }
+            })
+            //send
+            $('#sendbtn').click(function(){
                 postConecnt();
-            });
+            })
+
         }
     })
 })
+
+
+/*鍙戦€佹秷鎭�*/
+function left(headSrc,str){
+    var html="<div class='send'><div class='msg'><img src="+headSrc+" />"+
+        "<p><i class='msg_input'></i>"+str+"</p></div></div>";
+    upView(html);
+}
+/*鎺ュ彈娑堟伅*/
+function right(headSrc,str){
+    var html="<div class='show'><div class='msg'><img src="+headSrc+" />"+
+        "<p><i class='msg_input'></i>"+str+"</p></div></div>";
+    upView(html);
+}
+/*鏇存柊瑙嗗浘*/
+function upView(html){
+    $('.message').append(html);
+    $('body').animate({scrollTop:$('.message').outerHeight()-window.innerHeight},200)
+}
+function sj(max){
+    return parseInt(Math.random()*max)
+}
+var imgarr=['/static/img/touxiang.png','/static/img/touxiangm.png']
---
+++ static/js/welcome.js
@@ -0,0 +1,19 @@
+(function($) {
+    'use strict';
+
+    $(function() {
+        var $fullText = $('.admin-fullText');
+        $('#admin-fullscreen').on('click', function() {
+            $.AMUI.fullscreen.toggle();
+        });
+
+        $(document).on($.AMUI.fullscreen.raw.fullscreenchange, function() {
+            $fullText.text($.AMUI.fullscreen.isFullscreen ? '閫€鍑哄叏灞�' : '寮€鍚叏灞�');
+        });
+
+
+        var getWindowHeight = $(window).height(),
+            myappLoginBg    = $('.myapp-login-bg');
+        myappLoginBg.css('min-height',getWindowHeight + 'px');
+    });
+})(jQuery);
